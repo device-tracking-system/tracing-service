fb5d66c4826145506f32458f4340407a7638186f
bgrochal <bartek.grochal@gmail.com>
2018-01-12 01:16:58 +0100

    Integration with Configuration Service and Discovery Service. Implementation of Tracing Service.

--- src/main/java/pl/edu/agh/iet/dts/tracing/TracingService.java
+++ src/main/java/pl/edu/agh/iet/dts/tracing/TracingService.java
@@ -1,12 +1,20 @@
 package pl.edu.agh.iet.dts.tracing;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.EnableZipkinServer;
+
 /**
  * @author Bartłomiej Grochal
  */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
 public class TracingService {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(TracingService.class, args);
     }
 
 }
